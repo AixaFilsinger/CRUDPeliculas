69d7d0b0ba6761d7b3f71c6d0ad99eabad9ff396
Aixa Filsinger <macafil99@gmail.com>
2023-04-12 08:12:14 -0300

    probamos el evento click en el boton editar

--- js/administrador.js
+++ js/administrador.js
@@ -1,1 +1,12 @@
 import Pelicula from "./classPelicula.js";
+
+const btnEditar = document.querySelector('#btnEditar');
+
+
+function crearPeli(){
+    // crear una nueva peli
+    let nuevaPeli = new Pelicula('Super mario','algo','url','aventura',2023, '2hs','EEUU','-');
+    console.log(nuevaPeli)
+    
+    }
+    
